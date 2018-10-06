1a6e47c1b1fabd8fcce4979fd5b3e8c060cd9572
NigelKay <41966269+NigelKay@users.noreply.github.com>
2018-10-06 17:12:03 +0100

    Update Settings.py

--- snps/settings.py
+++ snps/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_KEY']
+SECRET_KEY = os.environ["DJANGO_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
