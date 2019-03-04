839bf0ba5a14006ee0c189a7ca43e0e07fc00073
Nigel Kay <41966269+NigelKay@users.noreply.github.com>
2019-03-04 21:48:02 +0000

    Update settings.py

--- snps/settings.py
+++ snps/settings.py
@@ -21,10 +21,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ***REMOVED***
+SECRET_KEY = os.environ["DJANGO_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.stnicholaspreschool.com', 'webapp-490223.pythonanywhere.com']
 
