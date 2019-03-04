181d347f026d1adcf26c01e1fda343c16a2d8d0e
NigelKay <kay.webdev@gmail.com>
2019-03-04 21:41:45 +0000

    Resync from PythonAnywhere

--- snps/settings.py
+++ snps/settings.py
@@ -21,12 +21,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_KEY']
+SECRET_KEY = ***REMOVED***
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.stnicholaspreschool.com', 'webapp-490223.pythonanywhere.com']
 
 
 # Application definition
